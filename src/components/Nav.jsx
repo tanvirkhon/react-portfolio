b22c5c11f8bed41503504afad9bb1a54dd38c03d
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-04 00:35:16 -0500

    Added components files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="mt-4 text-lg">
+      <section>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </section>
+    </nav>
+  );
+};
+
+export default Nav;
